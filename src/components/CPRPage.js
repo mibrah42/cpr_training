d74b4c0cc4ec334edfd7b5bfa1c59e33f17a0806
Samuel <Scroteau94@gmail.com>
2020-01-26 08:46:12 -0500

    add transform function

--- src/components/CPRPage.js
+++ src/components/CPRPage.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CPRDoll from './assets/cpr_doll.png';
-import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import MetricCard from './MetricCard';
 import Game from './Game';
+
+const io = require('socket.io-client');
+const ioClient = io.connect('http://localhost:8000');
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -15,7 +17,81 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-function CPRPage(props) {
+function CPRPage() {
+  const [distance, setDistance] = useState();
+  const [force, setForce] = useState(0); // aim for 60 lbf
+  const [bpm, setBpm] = useState(0); // aim for 100-120 bpm
+
+  const transform = (dataMin, dataMax, resultMin, resultMax, dataValue) => {
+    return (
+      ((dataValue - dataMin) / (dataValue - dataMax)) * resultMax + resultMin
+    );
+  };
+
+  // useEffect(() => {
+  //   var frequency = function(
+  //     maxFrequency,
+  //     milliseconds,
+  //     updateFrequency,
+  //     callback
+  //   ) {
+  //     var counter = 0,
+  //       clearTimer,
+  //       start,
+  //       delta;
+
+  //     var count = function() {
+  //       clearTimeout(clearTimer);
+
+  //       if (!start) {
+  //         start = new Date().getTime();
+  //       } else {
+  //         delta = new Date().getTime() - start;
+  //         time.innerText = delta / 1000;
+  //         tempo.value = Math.round((60 * 1000 * counter) / delta);
+
+  //         // A sec N times
+  //         // 60 sec X times
+  //         // X = N * 60 / A
+  //       }
+  //       counter++;
+
+  //       // Reset counter after 5 seconds
+  //       clearTimer = setTimeout(function() {
+  //         counter = 0;
+  //         delta = 0;
+  //         start = 0;
+  //         time.innerText = '';
+  //         tempo.value = '';
+  //       }, 5000);
+  //     };
+  //   };
+
+  //   frequency(60, 1000, 100, function(
+  //     milliseconds,
+  //     numberOfClicks,
+  //     targetPercentage
+  //   ) {
+  //     // document.getElementById('numberOfMilliseconds').innerHTML = milliseconds;
+  //     console.log('milliseconds', milliseconds);
+  //     // document.getElementById('numberOfClicks').innerHTML = numberOfClicks;
+  //     console.log('numberOfClicks', numberOfClicks);
+  //     // document.getElementById('targetPercentage').innerHTML = targetPercentage;
+  //     console.log('targetPercentage', targetPercentage);
+  //   });
+  // }, []);
+
+  useEffect(() => {
+    ioClient.on('pressDistance', pressDistance => {
+      console.info('pressDistance', pressDistance);
+      setForce(
+        pressDistance
+          ? Math.min(0, transform(550, 700, 0, 80, pressDistance))
+          : 0
+      );
+    });
+  }, []);
+
   const classes = useStyles();
   return (
     <div className={classes.root}>
@@ -45,7 +121,7 @@ function CPRPage(props) {
             }}
           >
             <MetricCard title="BPM (Beats per minute)" value="18.5" />
-            <MetricCard title="Force applied (lbs)" value="90 lbs" />
+            <MetricCard title="Force applied (lbs)" value={`${force} lbs`} />
           </div>
           <div
             style={{
